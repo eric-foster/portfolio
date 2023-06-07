c98571e59898de0beeda36e8a288bb1d7484f9e6
Eric Foster <etfoster.1001@yahoo.com>
2023-06-06 20:03:34 -0400

    Animated Navbar and Consolidated Code

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 // Navbar toggle
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('right-navbar')[0]
+const toggleButton = document.querySelector('.toggle-button');
+const navbarLinks = document.querySelector('.right-navbar');
 
 toggleButton.addEventListener('click', () => {
+    toggleButton.classList.toggle('active')
     navbarLinks.classList.toggle('active')
 })
 
+document.querySelectorAll(".right-navbar").forEach(n => n.addEventListener("click", () => {
+    toggleButton.classList.remove('active');
+    navbarLinks.classList.remove('active');
+}))
 // Contact Form
 
 function sendMail() {
