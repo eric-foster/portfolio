8ec76079f227d0adf37743311e72d91965169130
Eric Foster <etfoster.1001@yahoo.com>
2023-06-19 00:33:20 -0400

    disabled the contact form

--- index.js
+++ index.js
@@ -11,27 +11,3 @@ document.querySelectorAll(".right-navbar").forEach(n => n.addEventListener("clic
     toggleButton.classList.remove('active');
     navbarLinks.classList.remove('active');
 }))
-
-// Contact Form
-
-function sendMail() {
-    var params = {
-        name: document.getElementById('name').value,
-        email: document.getElementById('email').value,
-        message: document.getElementById('message').value,
-    };
-
-    const serviceID = 'service_pho2ilc'
-    const templateID = 'template_nzyvrie'
-
-    emailjs.send(serviceID, templateID, params)
-    .then(
-        (res) => {
-            document.getElementById("name").value = "";
-            document.getElementById("email").value = "";
-            document.getElementById("message").value = "";
-            console.log(res);
-            alert("Message Sent Successfully. Please frequently check your email for responses.");
-        })
-        .catch((err) => console.log(err));
-}
